5a86218cb55bf23748ced9330af0af5517cd06c0
ederr <ernest.derry@travisperkins.co.uk>
2017-08-04 13:41:06 +0100

    alternative versions of exercise 99

--- src/four_clojure/exercise99.clj
+++ src/four_clojure/exercise99.clj
@@ -8,13 +8,12 @@
         (recur (int (/ p 10)) (cons (mod p 10) result))))))
 
 
+(defn prodDigit2 [a b] 
+  (map #(Character/digit % 10) (seq (str (* a b)))))
 
+(defn prodDigit3 [a b] (map #(Integer/parseInt (str %)) (seq (str (* a b)))))
 
-
-(println (= [1] (prodDigit 1 1)))
-(println (= [1 0] (prodDigit 1 10)))
-(println (= [4 0 9 6] (prodDigit 64 64)))
-
-
-
+(println (= [1] (prodDigit3 1 1)))
+(println (= [1 0] (prodDigit3 1 10)))
+(println (= [4 0 9 6] (prodDigit3 64 64)))
 
